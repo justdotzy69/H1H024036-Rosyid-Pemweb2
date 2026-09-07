a4150e1bf13a8dda1ccb2e433e781e88097a749e
Khoirul Rosyid <khoirulrosyid135.com>
2026-09-07 12:13:12 +0700

    Utama

---
+++ pertemuan1/Latihan-Laravel/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LARAVEL</title>
+</head>
+<body>
+    <h1>Nama Lengkap : Khoirul Rosyid Gunawan </h1><br>
+    <h1>NIM : H1H024036 </h1>
+
+    
+</body>
+</html>
